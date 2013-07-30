2301392b5818541c3cf3f842696339a5b5d7c4c6
Baylee Feore <baylee.feore@gmail.com>
2013-07-30 15:23:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BalerionAir::Application.config.secret_token = 'f124209798684ffacb1a54cfaae31af008adee00850aa67a4c7ce09ce86cba4b31e7c5cdaf251588d528eea789d527f70ad0e62cbbbf5b47afb38a85df563979'
